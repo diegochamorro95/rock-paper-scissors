4ba94c763ba5763a39a219bd2e3de9b52bcba949
Diego <dchamorro95@gmail.com>
2025-05-28 16:02:46 -0300

    rps revision

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -1,3 +1,4 @@
+
 function getComputerChoice() {
     let option = Math.round(Math.random() * 100);
     if (option <= 33) {
@@ -10,66 +11,83 @@ function getComputerChoice() {
         return "scissors"
     }
 }
-
-
-function getHumanChoice() {
-    let option = prompt("Type your choice: ").toLowerCase(); 
-    return option; 
-}
+const humanPoints = document.querySelector("#humanPoints");
+const computerPoints = document.querySelector("#computerPoints");
+const resultText = document.querySelector("#resultText");
 var humanScore = 0;
 var computerScore = 0;
+let gameOver = false;
+
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == "paper" && computerChoice == "rock") {
-        console.log("You win! Paper beats rock");
+        resultText.innerText = "You win! Paper beats rock.";
         humanScore = humanScore +1;
     }
     else if (humanChoice == "paper" && computerChoice == "scissors") {
-        console.log("You lose! Scissors beats paper.");
+        resultText.innerText ="You lose! Scissors beats paper.";
         computerScore = computerScore +1;
     }
     else if (humanChoice == "paper" && computerChoice == "paper") {
-        console.log("Tie! Try again")
+        resultText.innerText ="Tie! Try again.";
     }
 
     else if (humanChoice == "rock" && computerChoice == "rock") {
-        console.log("Tie! Try again.");
+        resultText.innerText ="Tie! Try again.";
     }
     else if (humanChoice == "rock" && computerChoice == "scissors") {
-        console.log("You win! Rock beats scissors.");
+        resultText.innerText ="You win! Rock beats scissors.";
         humanScore = humanScore +1;
     }
     else if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log("You lose! Paper beats rock.");
+        resultText.innerText ="You lose! Paper beats rock.";
         computerScore = computerScore + 1;
     }
 
     else if (humanChoice == "scissors" && computerChoice == "rock") {
-        console.log("You lose! Rock beats scissors");
+        resultText.innerText ="You lose! Rock beats scissors.";
         computerScore = computerScore +1;
     }
     else if (humanChoice == "scissors" && computerChoice == "scissors") {
-        console.log("Tie! Try again.");
+        resultText.innerText ="Tie! Try again.";
     }
     else if (humanChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win! Scissors beats paper.");
+        resultText.innerText ="You win! Scissors beats paper.";
         humanScore = humanScore +1;
     }
-}
-
-var humanSelection = getHumanChoice();
-var computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-var round = 1;
-function playGame() {
-    while (round < 5) {
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
-        playRound(humanSelection,computerSelection);
-        round++;
+    humanPoints.textContent = `YOU: ${humanScore}`;
+    computerPoints.textContent = `COMPUTER: ${computerScore}`;
+    if (humanScore == 5) {
+        resultText.innerText = "GAME OVER! YOU WON!";
+        return;
+    } else if (computerScore == 5) {
+        resultText.innerText = "GAME OVER! YOU LOSE!";
+        return;
     }
-    console.log(`Final score: You: ${humanScore} vs Computer: ${computerScore}.`)
+
+}
+const rockButton = document.querySelector("#rockBtn");
+const paperButton = document.querySelector("#paperBtn");
+const scissorsButton = document.querySelector("#scissorsBtn");
+let humanChoice = "";
+function playGame() {
+    rockButton.addEventListener("click", () => {
+        playRound("rock", getComputerChoice());
+      
+    });
+
+    paperButton.addEventListener("click", () => {
+        playRound("paper", getComputerChoice());
+       
+    });
+
+    scissorsButton.addEventListener("click", () => {
+        playRound("scissors", getComputerChoice());
+       
+    });  
 }
 
 playGame();
+
+
+
